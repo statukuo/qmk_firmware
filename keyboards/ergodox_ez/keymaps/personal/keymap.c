7a3bfe96bb6e74f82442a08e70ec04299dd6d137
David Barbero <dbdstk@gmail.com>
2021-03-29 12:37:06 +0200

    Make symb key only on press

--- keyboards/ergodox_ez/keymaps/personal/keymap.c
+++ keyboards/ergodox_ez/keymaps/personal/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LGUI,         KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,    KC_LBRC,              KC_RBRC,      KC_Y,    KC_U,    KC_I,    KC_O,              KC_P,           KC_RGUI,
   KC_TAB,          KC_A,        KC_S,          KC_D,    KC_F,    KC_G,                                        KC_H,    KC_J,    KC_K,    KC_L,              KC_SCLN,        KC_QUOT,
   KC_DELT,         KC_Z,        KC_X,          KC_C,    KC_V,    KC_B,    KC_BSLS,              KC_SLSH,      KC_N,    KC_M,    KC_COMM, KC_DOT,            KC_UP,          KC_BSPC,
-  KC_LCTL,         KC_LALT,     KC_GRV,        TT(SYMB),KC_LPRN,                                                       KC_RPRN, TG(MDIA),KC_LEFT,           KC_DOWN,        KC_RGHT,
+  KC_LCTL,         KC_LALT,     KC_GRV,        MO(SYMB),KC_LPRN,                                                       KC_RPRN, TG(MDIA),KC_LEFT,           KC_DOWN,        KC_RGHT,
                                                                  KC_INS,  KC_HOME,              KC_PGUP,      KC_PSCR,
                                                                           KC_END,               KC_PGDN,
                                                         KC_SPC,  KC_LSFT, KC_CAPS,              KC_CAPS,      KC_RSFT, KC_ENT
